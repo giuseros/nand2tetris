2d9fcec6db3710a4c2d524e3f12649889b298b90
Giuseppe Rossini <giuseros85@gmail.com>
2022-08-23 17:28:47 +0100

    Add a makefile to the GEMM program to try it out

---
+++ GEMM/Makefile.sh
@@ -0,0 +1,12 @@
+export PYTHONPATH=/mnt/data/nand2tetris/
+IL=17
+ROM_SIZE=60000
+target=GEMM
+cp /mnt/data/nand2tetris/OS/*.jack .
+
+python3 -m Jack.JackCompiler .
+python3 -m VM.VMTranslator .
+python3 -m Hack.hack --il=$IL $target.asm > $target.bin
+python3 -m Hack.hack --il=$IL $target.asm > $target.hack
+python3 -m Verilog.convert_to_mif -d $ROM_SIZE -w $IL $target.bin $target.mif
+wc -l $target.asm
